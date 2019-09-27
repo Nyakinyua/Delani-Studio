aac7bce1caca304b461ef4cb000ed722dc8ef011
Nyakinyua <wanyakinyua968@gmail.com>
2019-09-27 18:23:30 +0300

    add file scripts.js

---
+++ js/sripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+ /* $(".design").click(function(){
+        $(".design1").toggle();
+        });*/(
+        $(".onClick").click(function()  {
+            $("#joy1").toggle();
+            $("#click").toggle();
+        });
+});
