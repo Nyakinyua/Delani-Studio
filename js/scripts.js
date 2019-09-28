a98b27ac6692c83a42d4ce23719fb9d5f8626030
Nyakinyua <wanyakinyua968@gmail.com>
2019-09-28 13:43:56 +0300

    rename js/{sripts.js-scripts.js}

--- js/scripts.js
+++ js/scripts.js
@@ -4,18 +4,19 @@ $(document).ready(function() {
         $(".para1").toggle(800);
         });
 $(".development").click(function() {
-         $(".para2").toggle();
-         $(".development").toggle();
+         $(".icon2").toggle(800);
+         $(".para2").toggle(800);
         });
 $(".product").click(function() {
-        $(".para3").toogle();
+        $(".icon3").toggle(800);
+        $(".para3").toggle(800);
         });
 
         
 
 
 //contact us form inputs
-        $("#blanks form").submit(function(event) {
+        /*$("#blanks form").submit(function(event) {
                 var personInput = $("input#contact-name").val();
                 var emailInput = $("input#contact-email").val();
                 var messageInput=$("input#contact-message").val();
@@ -27,6 +28,23 @@ $(".product").click(function() {
                 $("#message").show();
         
                 event.preventDefault();
-        });
+        });*/
+ $("#sbmt1").click(function(event)
+ {
+   event.preventDefault();
+ var name=$("#contact-name").val();
+var email=$("#contact-email").val();
+   if($("#user-name").val()==""){
+     alert("Please input name")
+   }
+   else if($("#contact-email").val()==""){
+     alert("Please input email")
+   }
+   else if($("#contact-message").val()==""){
+     alert("Please input your message")
+   }
+   else{
+   alert("Thank you "+ name + "," + " your messege has been received and is being acted on. .");}
+ });
    
 });
