260a8c469654c41841b0f4af709a772f33341fe4
Nyakinyua <wanyakinyua968@gmail.com>
2019-09-28 10:54:45 +0300

    add output message to inform the user of message reception

--- js/sripts.js
+++ js/sripts.js
@@ -1,9 +1,30 @@
-$(document).ready(function(){
- /* $(".design").click(function(){
-        $(".design1").toggle();
-        });*/(
-        $(".onClick").click(function()  {
-            $("#joy1").toggle();
-            $("#click").toggle();
+$(document).ready(function() {
+ $(".design").click(function() {
+        $(".para1").toggle();
         });
+$(".development").click(function() {
+         $(".para2").toggle();
+        });
+$(".project").click(function() {
+        $(".para3").toggle();
+        });
+
+        
+
+
+//contact us form inputs
+        $("#blanks form").submit(function(event) {
+                var personInput = $("input#contact-name").val();
+                var emailInput = $("input#contact-email").val();
+                var messageInput=$("input#contact-message").val();
+
+                $(".contact-name").text(personInput);
+                $(".contact-email").text(emailInput);
+                $(".contact-message").text(messageInput);
+
+                $("#message").show();
+        
+                event.preventDefault();
+        });
+   
 });
